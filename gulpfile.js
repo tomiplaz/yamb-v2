c59bfad180a17656a551a940489c3425a08784ba
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2016-11-02 22:46:24 +0100

    Define gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,38 @@
-const elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var pump = require('pump');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+var jsFiles = [
+    'resources/assets/js/bower_components/angular/angular.js',
+    'resources/assets/js/bower_components/angular-ui-router/angular-ui-router.js',
+    'resources/assets/js/main.js'
+];
 
-elixir(mix => {
-    mix.sass('app.scss')
-       .scripts([
-           'bower_components/angular/angular.js',
-           'bower_components/angular-ui-router/release/angular-ui-router.js',
-           'app.js'
-       ], 'public/js/app.js');
+var scssFiles = [
+    'resources/assets/sass/main.scss'
+];
+
+gulp.task('js', function(cb) {
+    pump([
+        gulp.src(jsFiles),
+        concat('app.js'),
+        gulp.dest('public/js')
+    ], cb);
+});
+
+gulp.task('sass', function(cb) {
+    pump([
+        gulp.src(scssFiles),
+        sass(),
+        concat('app.css'),
+        gulp.dest('public/css')
+    ], cb);
+});
+
+gulp.task('watch', ['js', 'sass'], function() {
+    gulp.watch(jsFiles, ['js']);
+    gulp.watch(scssFiles, ['sass']);
 });
