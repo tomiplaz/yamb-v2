b31ad1dbea882152abbf2a794a301885dec2fca4
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2016-12-14 22:33:04 +0100

    Add backend validation for user registration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterUser;
 use App\User;
 
 class UserController extends Controller
@@ -23,7 +24,7 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(RegisterUser $request)
     {
         return User::create($request->input());
     }
---
+++ app/Http/Requests/RegisterUser.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUser extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|size:20|alpha_num',
+            'email' => 'required|email|size:100|unique:users',
+            'password' => 'required'
+        ];
+    }
+}
